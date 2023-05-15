7c8a2711c2ddc10a9f58f2eb3e45ef025c6e6e91
grigoriirarog <rarog.grigoriii@gmail.com>
2023-05-15 13:43:09 +0200

    Решение задачи 4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String[] array = {"bb", "aa", "cc", "bb", "ff", "aa", "gg"};
+        String strResult = removeDuplicates(array);
+        System.out.println(strResult);
+    }
+
+    public static String removeDuplicates(String[] array) {
+        Set<String> str = new LinkedHashSet<>();
+        String string = "";
+        for (String str2 : array) {
+            str.add(str2);
+        }
+        string = String.join(" ", str);
+        return string;
     }
 }
